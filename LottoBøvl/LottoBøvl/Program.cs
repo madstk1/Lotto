a3681629c9321554c7ab6ffa3fb92243efd8806b
Mathias Mebus <math446c@student.eadania.dk>
2019-11-12 21:52:11 +0100

    ny

--- LottoBøvl/LottoBøvl/Program.cs
+++ LottoBøvl/LottoBøvl/Program.cs
@@ -1,37 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LottoBøvl
+
+namespace LottoKuppon
 {
     class Program
     {
-        public int[] coupon;
-
         static void Main(string[] args)
         {
             //Lav en eller anden form for menu til at vælge om du vil have lavet en kuppon eller noget andet(??????)
-            Console.WriteLine("Lotto " + DateTime.Now.ToString("dd.MM.yyy"));
-            // update fra bærbar
+            Console.WriteLine("     Lotto " + DateTime.Now.ToString("dd.MM.yyy"));
+            Console.WriteLine();
+            Console.WriteLine("           1-uge");
+            Console.WriteLine("         LYN-LOTTO");
+            Console.WriteLine();
 
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(" " + i + ".  " + CreateRow());
+            }
 
-            //Console.WriteLine(NumberGenerator());
-            Console.WriteLine(CreateRow());
+            Console.WriteLine("10.  " + CreateRow());
 
             Console.ReadKey();
         }
-
 
         public static string CreateRow()
         {
             string row = "";
             int[] rowArray = new int[7];
-            Random randomNumber = new Random();
 
             for (int i = 0; i < 7; i++)
             {
+                Random randomNumber = new Random();
                 int number = randomNumber.Next(1, 37);
                 int foundResult = Array.IndexOf(rowArray, number);
 
@@ -41,17 +41,28 @@ namespace LottoBøvl
                 }
                 else
                 {
+                    //DET EN OMMER!
                     i--;
                 }
             }
             Array.Sort(rowArray);
 
-            foreach (var item in rowArray)
+            foreach (int number in rowArray)
             {
-                row += (item + " ");
+                if (number > 9)
+                {
+                    row += (number + " ");
+                }
+                else
+                {
+                    row += ("0" + number + " ");
+                }
             }
 
             return row;
         }
+
+
     }
 }
+
